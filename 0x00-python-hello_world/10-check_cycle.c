5e37175730bb67286139a3405ecdd7c312cafce3
root <jesus4388>
2022-04-25 14:39:13 -0700

    print

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,7 +1,23 @@
 #include "lists.h"
+#include <stddef.h>
 /**
  */
 int check_cycle(listint_t *list)
 {
+	listint_t *aux = list;
+	listint_t *aux2 = list;
 
+	while(aux2 != NULL)
+	{
+		aux = aux->next;
+		aux2 = aux->next->next;
+		if (aux == aux2)
+			break;
+		if (aux2 == NULL)
+			break;
+	}
+	if (aux == aux2)
+		return(1);
+	else
+		return(0);
 }
