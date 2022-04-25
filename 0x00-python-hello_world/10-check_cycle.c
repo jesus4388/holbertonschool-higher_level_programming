d4a1e63cd240b2caa7db3e1de04d1c17b3232f51
root <jesus4388>
2022-04-25 16:26:52 -0700

    node

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,18 +1,24 @@
 #include "lists.h"
 #include <stddef.h>
 /**
+ * check_cycle - check_cycle
+ * @list: pointer
+ * Return: 0 or 1
  */
 int check_cycle(listint_t *list)
 {
 	listint_t *aux = list;
 	listint_t *aux2 = list;
 
-	while(aux2)
+	if (list && aux)
 	{
-		aux = aux->next;
-		aux2 = aux2->next->next;
-		if (aux == aux2)
-			return(1);
+		while (aux2)
+		{
+			aux = aux->next;
+			aux2 = aux2->next->next;
+			if (aux == aux2)
+				return (1);
+		}
 	}
-	return(0);
+	return (0);
 }
