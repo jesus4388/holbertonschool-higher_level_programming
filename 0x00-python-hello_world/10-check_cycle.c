78cf841269609800ade4694135ce5af2440204dd
root <jesus4388>
2022-04-25 16:33:46 -0700

    node

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -7,18 +7,14 @@
  */
 int check_cycle(listint_t *list)
 {
-	listint_t *aux = list;
-	listint_t *aux2 = list;
+	listint_t *aux = list, *aux2 = list;
 
-	if (list && aux)
+	while (aux2 && aux && list)
 	{
-		while (aux2)
-		{
-			aux = aux->next;
-			aux2 = aux2->next->next;
-			if (aux == aux2)
-				return (1);
-		}
+		aux = aux->next;
+		aux2 = aux2->next->next;
+		if (aux == aux2)
+			return (1);
 	}
 	return (0);
 }
