82ae3500fbc43ae70e15bf9615f8e275333ed015
root <jesus4388>
2022-04-26 15:09:13 -0700

    digit

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for i in str:
+        num = ord(i)
+        if(num >= 97 and num < 123):
+            num = num - 32
+            l = chr(num)
+            print(f"{i}", end="")
---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def print_last_digit(number):
+    if (number < 0):
+        number = number * -1
+        num = number % 10
+        print (f"{number % 10}", end="")
+    else:
+        print (f"{number % 10}", end="")
+        num = number % 10
+    return num
