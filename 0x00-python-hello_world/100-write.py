f78d224d3873ecde27ab2890e60400ab2fc35beb
root <jesus4388>
2022-04-25 17:53:58 -0700

    write

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-text = "and that piece of art is useful - Dora Korpar, 2015-10-19"
 import sys
-sys.stderr.write(str(text) + '\n')
+sys.stderr.write('and that piece of art is useful - Dora Korpar, 2015-10-19\n')
 exit(1)
