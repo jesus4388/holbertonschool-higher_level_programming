aba98bc74da2536fffba402529e8c102547bc2b0
root <jesus4388>
2022-04-28 10:59:35 -0700

    args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-import sys
-n = len(sys.argv)
-if n == 1:
-    print(f"0 arguments.")
-elif n == 2:
-    print(f"1 argument:")
-else:
-    print(f"{n - 1} arguments:")
-for i in range(1, n):
-    print(f"{i}: {sys.argv[i]}")
+if __name__ == "__main__":
+    import sys
+    n = len(sys.argv)
+    if n == 1:
+        print(f"0 arguments.")
+    elif n == 2:
+        print(f"1 argument:")
+    else:
+        print(f"{n - 1} arguments:")
+    for i in range(1, n):
+        print(f"{i}: {sys.argv[i]}")
