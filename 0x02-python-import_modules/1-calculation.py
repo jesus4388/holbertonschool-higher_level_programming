b8b5a0782db55de47543d5f74590a9f3b3d251cd
root <jesus4388>
2022-04-28 10:13:30 -0700

    calculation

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -3,22 +3,16 @@ if __name__ == "__main__":
     from calculator_1 import add
     a = 10
     b = 5
-print(f"{a} + {b} = {add(a, b)}")
-
-if __name__ == "__main__":
+    print(f"{a} + {b} = {add(a, b)}")
     from calculator_1 import sub
     a = 10
     b = 5
-print(f"{a} - {b} = {sub(a, b)}")
-
-if __name__ == "__main__":
+    print(f"{a} - {b} = {sub(a, b)}")
     from calculator_1 import mul
     a = 10
     b = 5
-print(f"{a} * {b} = {mul(a, b)}")
-
-if __name__ == "__main__":
+    print(f"{a} * {b} = {mul(a, b)}")
     from calculator_1 import div
     a = 10
     b = 5
-print(f"{a} / {b} = {div(a, b)}")
+    print(f"{a} / {b} = {div(a, b)}")
