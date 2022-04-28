1ad18e91acc9fbe5f97daef57125df6feda61398
root <jesus4388>
2022-04-28 15:02:22 -0700

    load

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,15 +1,11 @@
 #!/usr/bin/python3
 import sys
 add = 0
-addr = 0
 if __name__ == "__main__":
     n = len(sys.argv) - 1
-    if n == 0:
+    if n == 1:
         print(f"0")
     for i in range(1, n):
-        if (int(sys.argv[i]) < 0):
-            add -= int(sys.argv[i])
-        if (int(sys.argv[i]) > 0):
-            addr += int(sys.argv[i])
-    add = add - addr
+        if (int(sys.argv[i]) >= 0 and int(sys.argv[i]) <= 9):
+            add += int(sys.argv[i])
     print(f"{add}")
