8077914d1f47a4c18e5c132b77a3c4fea38f2b27
root <jesus4388>
2022-04-28 16:19:31 -0700

    infinite

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -9,6 +9,7 @@ if __name__ == "__main__":
     elif n > 2:
         for i in range(1, n):
             add += int(sys.argv[i])
+            i = i + 1
             print(f"{add}")
     else:
         print(0)
