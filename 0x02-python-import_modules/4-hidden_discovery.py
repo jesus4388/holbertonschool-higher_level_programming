c6b9eeddebe0545b6fc1852db366ed520172fd92
root <jesus4388>
2022-04-28 15:52:57 -0700

    hidden

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-import hidde_4
+import hidden_4
 if __name__ == "__main__":
-    lis = dir(hidde_4)
+    lis = dir(hidden_4)
     for i in lis:
         if (i[0] != '_' and i[1] != '_'):
             print(f"{i}")
