5c4883e38cf6c79971a8f473f94394985fef247b
root <jesus4388>
2022-04-29 08:22:31 -0700

    multi

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     arg = len(sys.argv)
     if(arg != 4):
         print(f"Usage: ./100-my_calculator.py <a> <operator> <b>")
-        exit(1);
+        exit(1)
     else:
         a = int(sys.argv[1])
         b = int(sys.argv[3])
@@ -19,4 +19,4 @@ if __name__ == "__main__":
             print(f"{a} / {b} = {div(a, b)}")
         else:
             print("Unknown operator. Available operators: +, -, * and /")
-            exit(1);
+            exit(1)
