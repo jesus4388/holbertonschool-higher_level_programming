3d9678b8063d66744eb362af215e44f3ac8e026a
root <jesus4388>
2022-05-02 12:23:26 -0700

    string

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    for i in my_string:
-        if i == 'c' or i == 'C':
-            i = ""
+    characater = "Cc"
+    my_string = ''.join(x for x in my_string if x not in characater)
     return my_string
