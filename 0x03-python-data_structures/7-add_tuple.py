c2044c21af0dd8edb8814d3e87a9a52eba71a35a
root <jesus4388>
2022-05-02 15:24:17 -0700

    add

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -9,9 +9,9 @@ def add_tuple(tuple_a=(), tuple_b=()):
     else:
         aux1 = tuple_b[0], tuple_b[1]
     if len(tuple_a) == 0:
-        aux2 = tuple_a[1], 0
+        aux2 = 0, 0
     elif len(tuple_a) == 1:
-        aux2 = tuple_a[0], tuple_a[1]
+        aux2 = tuple_a[0], 0
     else:
         aux2 = tuple_a[0], tuple_a[1]
     return(aux1[0] + aux2[0], aux1[1] + aux2[1])
