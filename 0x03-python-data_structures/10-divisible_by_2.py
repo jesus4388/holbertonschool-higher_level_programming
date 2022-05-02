40aa80c3eae8671158d85011cae14f6787d0e305
root <jesus4388>
2022-05-02 16:48:37 -0700

    division

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    if my_list:
+        copy = my_list[:]
+        for i in my_list:
+            if i % 2 == 0:
+                copy[i] = True
+            else:
+                copy[i] = False
+        return copy
+    else:
+        return None
