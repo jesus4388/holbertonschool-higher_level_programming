2359653e6230498aaac2f923672361f9d8a9d3d9
root <jesus4388>
2022-05-02 17:15:11 -0700

    delete

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
     if my_list:
-        a = len(my_list) - 1
+        a = len(my_list)
         if (idx < 0 or idx > a):
             return my_list
         del my_list[idx]
