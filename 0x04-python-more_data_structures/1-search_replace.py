d66c00677e802fe313a2895c53e6ce84a4db9def
root <jesus4388>
2022-05-04 08:59:52 -0700

    replace

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,8 +2,8 @@
 def search_replace(my_list, search, replace):
     new = []
     for i in my_list:
-        if i == 2:
-            new.append(89)
+        if i == search:
+            new.append(replace)
         else:
             new.append(i)
     return new
