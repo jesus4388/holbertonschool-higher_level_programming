49aefb807f5eb2bafe5c1e0c1b30f2b066ec213a
root <jesus4388>
2022-05-04 11:15:25 -0700

    only

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,16 +1,10 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
     new = []
-    delete = []
     for i in set_1:
-        new.append(i)
+        if i not in set_2:
+            new.append(i)
     for j in set_2:
-        if j not in new:
+        if j not in set_1:
             new.append(j)
-        else:
-            delete.append(j)
-    for i in new:
-        for j in delete:
-            if i == j:
-                new.remove(i)
     return new
