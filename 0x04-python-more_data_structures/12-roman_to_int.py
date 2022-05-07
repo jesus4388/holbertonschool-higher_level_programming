22e50cd7c201bccadcca067794f8a7d7af70b87a
root <jesus4388>
2022-05-06 19:10:25 -0700

    roma

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,35 +1,15 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     b = 0
-    V = 0
-    X = 0
-    L = 0
-    C = 0
-    D = 0
-    c = 0
-    a = 1
-    M = 0
+    a = 0
+    roma = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     if roman_string is None or type(roman_string) != str:
         return 0
-    if roman_string:
-        for j in roman_string:
-            if j == 'I':
-                c = 1
-            if j == 'V':
-                c = 5
-            if j == 'X':
-                c = 10
-            if j == 'L':
-                c = 50
-            if j == 'C':
-                c = 100
-            if j == 'D':
-                c = 500
-            if j == 'M':
-                c = 1000
-            if a < c:
-                b = c - b
-                a = c
-            else:
-                b = b + c
+    for j in roman_string:
+            for i in roma:
+                if i == j:
+                    if a < roma.get(i):
+                        b = b - (a*2)
+                    b = b + roma.get(i)
+                    a = roma.get(i)
     return b
