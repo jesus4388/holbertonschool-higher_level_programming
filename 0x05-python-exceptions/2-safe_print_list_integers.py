3b520261db4e541550bf42e5318e098905337189
root <jesus4388>
2022-05-16 11:42:53 -0700

    list

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    b = 0
+    for i in range(0, x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            b = b + 1
+        except ValueError:
+            print("", end="")
+        except TypeError:
+            print("", end="")
+    print ()
+    return b
