4e19580864daebc37ef602d1115526a4c1ef0dec
root <jesus4388>
2022-05-16 18:07:12 -0700

    rase

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -3,6 +3,6 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
-    except Exception as e:
-        print(e)
+    except Exception as err:
+        print("Exception: {}".format(err))
         return False
