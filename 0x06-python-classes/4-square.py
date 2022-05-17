c6a94574aedc03c125898f9e20761154f242efe5
root <jesus4388>
2022-05-17 13:13:46 -0700

    property setter

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
+'# a class Square that defines a square'
+
+
 class Square:
+    '# Private instance attribute: size'
     def __init__(self, size=0):
+        '# size and type verification'
         if type(size) is not int:
             raise TypeError('size must be an integer')
         if size < 0:
@@ -8,13 +13,16 @@ class Square:
         else:
             self.__size = size
 
+    '# Método de instancia pública'
     def area(self):
         return self.__size * self.__size
 
+    '# property def size(self): to retrieve it'
     @property
     def size(self):
         return self.__size
 
+    '# property setter def size(self, value): to set it'
     @size.setter
     def size(self, val):
         if type(val) is not int:
