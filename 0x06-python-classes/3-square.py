9b9ef180f55964896986ceb217e9b10956391d41
root <jesus4388>
2022-05-17 13:04:22 -0700

    Public instance method

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
+'# a class Square that defines a square'
+
+
 class Square:
+    '# Private instance attribute: size'
     def __init__(self, size=0):
+        '# size and type verification'
         if not(type(size) == int):
             raise TypeError('size must be an integer')
         if size < 0:
@@ -8,5 +13,7 @@ class Square:
         else:
             self._Square__size = size
 
+    """Public instance method: def area(self): \
+            that returns the current square area"""
     def area(self):
         return self._Square__size * self._Square__size
