07e3af047bfb28f312be9b618ab9e5be886f7627
root <jesus4388>
2022-05-17 12:50:03 -0700

    instantiation with size

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+'# a class Square that defines a square'
+
+
 class Square:
+    '# Private instance attribute: size'
     def __init__(self, size):
         self._Square__size = size
