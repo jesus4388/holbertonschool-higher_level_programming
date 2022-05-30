9912e232995dc258e784c8b4f396cda7562c3953
root <jesus4388>
2022-05-30 16:05:22 -0700

    Square

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
+'# class Rectangle that inherits from BaseGeometryi'
+
 
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
+'# import '
+
 
 class Rectangle(BaseGeometry):
+    '# class Rectangle that inherits from BaseGeometryi'
 
     def __init__(self, width, height):
 
@@ -11,8 +16,10 @@ class Rectangle(BaseGeometry):
         self.__width = width
         self.__height = height
 
+    '# validator of area'
     def area(self):
         return self.__width * self.__height
 
+    'concat widch and height'
     def __str__(self):
         return "[Rectangle] " + str(self.__width) + "/" + str(self.__height)
