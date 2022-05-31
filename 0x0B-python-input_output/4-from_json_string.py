1b26c736889fb738cb86311123c88ab0012fd982
root <jesus4388>
2022-05-31 14:17:35 -0700

    json

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -2,10 +2,11 @@
 '# a function that returns an object (Python data structure)\
         represented by a JSON string'
 
+
 import json
+
 
 def from_json_string(my_str):
     '# return an objects'
 
     return json.loads(my_str)
-
