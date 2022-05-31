4e61f3703632975210e1715baad35bf3de2d7489
root <jesus4388>
2022-05-31 14:57:33 -0700

    save object to a file

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
+'# a function that creates an Object from a “JSON file”'
 
 
 import json
 
 
 def load_from_json_file(filename):
+    '# creat an onject'
+
     with open(filename, 'r') as f:
         _read = f.read()
     return json.loads(_read)
