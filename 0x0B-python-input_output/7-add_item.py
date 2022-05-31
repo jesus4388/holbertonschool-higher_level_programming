4d0d846f1b93a43eb6decce58bb91599543a3ff8
root <jesus4388>
2022-05-31 15:28:59 -0700

    load

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,9 +1,18 @@
 #!/usr/bin/python3
+'# a script that adds all arguments to a Python lisi'
+
 
 from sys import argv
+
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-item = load_from_json_file("add_item.json")
-save_to_json_file(item + argv[1], "add_item.json")
+
+try:
+    item = load_from_json_file("add_item.json")
+except Exception:
+    item = []
+
+item += argv[1:]
+save_to_json_file(item, "add_item.json")
