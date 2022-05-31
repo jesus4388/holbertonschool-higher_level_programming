5bb4217bc6bb2461446e1075b99a620729f838b5
root <jesus4388>
2022-05-31 14:53:19 -0700

    save object to a file

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -9,6 +9,5 @@ import json
 def save_to_json_file(my_obj, filename):
     '# object to a text file'
 
-    dict_json = json.dumps(my_obj, ensure_ascii=False)
     with open(filename, 'w') as f:
-        f.write(dict_json)
+        json.dump(my_obj, f)
