bad4deb78efd8cbe57386e8293799cc95182f034
root <jesus4388>
2022-05-31 14:12:53 -0700

    json

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
+'# a function that appends a string at the end of a text file'
 
 
 def append_write(filename="", text=""):
+    '# appends a string at the end of a text'
 
     with open(filename, "a") as f:
         f.write(text)
