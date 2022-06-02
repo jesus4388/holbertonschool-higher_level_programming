e8aeedf9dd36713ceed35050ff9f12d1e2c92e64
root <jesus4388>
2022-06-01 18:30:49 -0700

    key

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -2,31 +2,28 @@
 '# Write a class Student that defines a student by'
 
 
+import json
+
 class Student:
 
     def __init__(self, first_name, last_name, age):
-
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
+        dict1 = {}
         if type(attrs) is not list:
             return self.__dict__
-        if attrs is None:
+        if all(isinstance(_str, str)for _str in attrs):
+            for key, value in self.__dict__.item():
+                if key in attrs:
+                    dict1[key] = value
+            return dict1
+        else:
             return self.__dict__
-        for x in attrs:
-            if type(x) is str:
-                return self.__dict__
-
-        _dict = {}
-        for key, value in self.__dict__.item():
-            for x in attrs:
-                if key == x:
-                    _dict.update({key: value})
 
     def reload_from_json(self, json):
-        for key, value in json.item():
-            for i, j in self.__dict__.item():
-                if key == i:
-                    self.__dict__.update({key: value})
+        for key in self.__dict__:
+            self.__dict__[key] = json[key]
+        return self.__dict__
