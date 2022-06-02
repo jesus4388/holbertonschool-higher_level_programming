b6f94acef7a48558fd7e13b58873ba33bcaa7bb9
root <jesus4388>
2022-06-01 19:40:09 -0700

    pascal

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+'# returns a list of lists of integers\
+        representing the Pascal’s triangle'
+
+
+def pascal_triangle(n):
+    if n > 0:
+        lists = []
+        for i in range(n):
+            row = [1]
+            if i > 0:
+                for x in range(i):
+                    row.append(sum(lists[-1][x:x+2]))
+            lists.append(row)
+        return lists
+    else:
+        return []
