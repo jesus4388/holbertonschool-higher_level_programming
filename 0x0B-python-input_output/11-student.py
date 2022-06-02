79dc3f0add55f6150f1738a4a3444820a3e7fb24
root <jesus4388>
2022-06-01 18:35:05 -0700

    key

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -8,11 +8,15 @@ import json
 class Student:
 
     def __init__(self, first_name, last_name, age):
+        '# attributes'
+
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
+        '# retrieves a dictionary'
+
         dict1 = {}
         if type(attrs) is not list:
             return self.__dict__
@@ -25,6 +29,8 @@ class Student:
             return self.__dict__
 
     def reload_from_json(self, json):
+        '#replaces all atributes'
+
         for key in self.__dict__:
             self.__dict__[key] = json[key]
         return self.__dict__
