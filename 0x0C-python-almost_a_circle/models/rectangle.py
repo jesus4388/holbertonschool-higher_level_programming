aba4b1439628faef5e3cc6c90ef6c3baa990000f
jesus4388 <jesushernandez1843@gmail.com>
2022-06-06 10:56:23 -0700

    First Rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -126,4 +126,5 @@ class Rectangle(Base):
 
     def to_dictionary(self):
         '# assign an argument to each attribute'
-        return {'x': self.__x, 'y': self.__y, 'id': self.id, 'height': self.__height, 'width': self.__width}
+        return f"'x': {self.__x} 'y': {self.__y} 'id': {self.id}\
+ 'height': {self.__height}, 'width': {self.__width}"
