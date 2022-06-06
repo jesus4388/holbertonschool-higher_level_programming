2f392ab6805bfca703f96709e28bbb471fac7c35
jesus4388 <jesushernandez1843@gmail.com>
2022-06-06 13:00:25 -0700

    Rectangle instance to dictionary representation

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -127,5 +127,5 @@ class Rectangle(Base):
 
     def to_dictionary(self):
         '# assign an argument to each attribute'
-        return {'x': self.x, 'y': self.y, 'id': self.id,\
- 'height': self.height, 'width': self.width}
+        return {'x': self.x, 'y': self.y, 'id': self.id, 'height':
+                self.height, 'width': self.width}
