e55aea56fcd4bea7e006ed1882c7f3c41274be6d
jesus4388 <jesushernandez1843@gmail.com>
2022-06-06 13:43:32 -0700

    a

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -21,6 +21,7 @@ class Base:
         else:
             self.id = id
 
+    @staticmethod
     def to_json_string(list_dictionaries):
         '# return representation string of json'
         if list_dictionaries is None:
