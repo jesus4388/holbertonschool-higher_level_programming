3793ed076fa9b6b0877585af58ea6f95879a4f59
root <jesus4388>
2022-07-05 19:05:41 -0700

    my name

---
+++ 0x0D-SQL_introduction/14-average.sql
@@ -0,0 +1,2 @@
+-- a script that computes the score average
+SELECT AVG(score) 'average' FROM second_table;
---
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -0,0 +1,2 @@
+-- a script that lists the number of records
+SELECT score, COUNT(score) AS NUMBER from second_table GROUP BY score ORDER BY NUMBER DESC;
