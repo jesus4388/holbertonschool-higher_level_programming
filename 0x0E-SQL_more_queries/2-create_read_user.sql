f4fea10692f76ae3503bdcc823a3aa4ddbe5d8fd
root <jesus4388>
2022-07-06 12:29:53 -0700

    local

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,4 @@
 -- a script that creates the database
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'password';
 GRANT SELECT ON *.* TO 'user_0d_2'@'localhost';
