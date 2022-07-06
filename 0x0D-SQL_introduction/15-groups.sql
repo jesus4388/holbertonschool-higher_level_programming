083b1ea8ca63b136a2eb4c235e932fc290e68d6f
root <jesus4388>
2022-07-05 19:09:29 -0700

    groups

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,2 +1,3 @@
 -- a script that lists the number of records
-SELECT score, COUNT(score) AS number from second_table GROUP BY score ORDER BY number DESC;
+SELECT score, COUNT(score) AS NUMBER
+from second_table GROUP BY score ORDER BY NUMBER DESC;
