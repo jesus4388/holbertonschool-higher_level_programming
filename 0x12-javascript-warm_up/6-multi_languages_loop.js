8aa2b36d79ddc45d125dcc23d0e154eef65b8059
jesus4388 <jesushernandez1843@gmail.com>
2022-08-01 15:37:56 -0300

    loop

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,11 +1,8 @@
 #!/usr/bin/node
 let i = 0;
-let array = [
-  'C is fun',
-  'Python is cool',
-  'JavaScript is amazing'
-]
-while(i < array.length) {
+const array = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+while (i < array.length) {
   console.log(array[i]);
   i++;
 }
