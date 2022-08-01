712826fd6edb45010d1c08bbbc21f0ca0f3792d7
jesus4388 <jesushernandez1843@gmail.com>
2022-08-01 15:26:06 -0300

    arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,6 +2,9 @@
 if (process.argv.length === 2) {
   console.log('No argument');
 }
-if (process.argv.length > 2) {
+if (process.argv.length === 3) {
   console.log('Argument found');
+}
+if (process.argv.length > 3) {
+  console.log('Arguments found');
 }
