e3fd349bbc95b199b7dd26b03b1c85384390558c
jesus4388 <jesushernandez1843@gmail.com>
2022-08-02 17:43:08 -0300

    reverse

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,4 +1,8 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
-  return list.reverse();
+  const array = [];
+  for (let i = list.length - 1, j = 0; i >= 0; i--, j++) {
+    array[j] = list[i];
+  }
+  return array;
 };
