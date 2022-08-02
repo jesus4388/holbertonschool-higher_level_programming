cb1c20ba8d329c7d3917aca1530e313a7f9a9bcd
jesus4388 <jesushernandez1843@gmail.com>
2022-08-02 15:33:52 -0300

    square

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -3,7 +3,8 @@ const Rectangle = require('./4-rectangle');
 
 module.exports = class Square extends Rectangle {
   constructor (size) {
-    super(size, size);
-    this.size = size;
+    if (size > 0){
+      super(size, size);
+    }
   }
 };
