318531dabdc3376288884f78dc52fefab71a8ec6
root <jesus4388>
2022-08-07 08:35:12 -0700

    delete

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python3
-'# a script that changes the name of a State object'
+'# a script that deletes all State objects with a name'
 
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine
-from sqlalchemy import update
 from model_state import Base, State
 from sys import argv
 
 if __name__ == "__main__":
-
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
         argv[1], argv[2], argv[3]), pool_pre_ping=True)
 
     session = Session(engine)
-    for i in session.query(State).filter(State.id == 2):
-        i.name = "New Mexico"
+    for i in session.query(State).all():
+        for x in i.name:
+            if x == 'a':
+                session.delete(i)
 
     session.commit()
     session.close()
