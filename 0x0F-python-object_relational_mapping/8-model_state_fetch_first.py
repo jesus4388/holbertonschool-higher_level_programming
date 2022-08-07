21b11dcce6e4325447e58792812f85aba47b6660
root <jesus4388>
2022-08-06 19:01:32 -0700

    first

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -12,8 +12,10 @@ if __name__ == "__main__":
         argv[1], argv[2], argv[3]), pool_pre_ping=True)
 
     session = Session(engine)
-    for i in session.query(State).order_by(State.id).all():
-        print("{}: {}".format(i.id, i.name))
-        break
+    resultado = session.query(State).first()
+    try:
+        print("{}: {}".format(resultado.id, resultado.name))
+    except Exception:
+        print(Nothing)
 
     session.close()
