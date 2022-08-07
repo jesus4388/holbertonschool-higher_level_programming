9529affac0fc96e330e8b957f7c725fc8e72ab2a
root <jesus4388>
2022-08-06 18:29:06 -0700

    model

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -10,10 +10,10 @@ if __name__ == "__main__":
 
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
         argv[1], argv[2], argv[3]), pool_pre_ping=True)
-    
+
     session = Session(engine)
     for i in session.query(State).order_by(State.id).all():
         print("{}: {}".format(i.id, i.name))
-        break;
+        break
 
     session.close()
