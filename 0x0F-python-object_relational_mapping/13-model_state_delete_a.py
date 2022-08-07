6e2f850070191de6983132b8b480e48c3e2d8246
root <jesus4388>
2022-08-07 09:57:40 -0700

    l

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -2,7 +2,7 @@
 '# a script that deletes all State objects with a name'
 
 from sqlalchemy.orm import Session
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, update
 from model_state import Base, State
 from sys import argv
 
@@ -11,10 +11,7 @@ if __name__ == "__main__":
         argv[1], argv[2], argv[3]), pool_pre_ping=True)
 
     session = Session(engine)
-    for i in session.query(State).order_by(State.id).all():
-        for x in i.name:
-            if x == 'a' or x == 'A':
-                session.delete(i)
-
+    session.query(State).filter(State.name.contains('a%')).delete(
+            synchronize_session='fetch')
     session.commit()
     session.close()
