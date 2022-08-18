3b939e8631e693a01353bdac16ea85fbdf7492be
root <jesus4388>
2022-08-18 16:48:34 -0700

    l

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Bash script that takes in a URL, sends a request to that URL
-echo -sI "$1" | grep -i "Content-Lenth" | awk "{ print $2 }"
+echo -sI "$1" | grep -i Content-Lenth | awk "{print $2}"
