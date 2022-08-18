4aeefabf0999aa9d90fc493121c3564eb641b262
root <jesus4388>
2022-08-18 16:35:40 -0700

    l

---
+++ 0x10-python-network_0/0-body_size.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/bash
+#Bash script that takes in a URL, sends a request to that URL
+echo -sI "$1" | grep -i "Content-Lenth" | '{ print $2}'
---
+++ 0x10-python-network_0/1-body.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/bash
+#Bash script that takes in a URL
+curl -s GET "$1"
