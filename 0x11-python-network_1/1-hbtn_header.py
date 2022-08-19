a270db0bc6f81796b6698bf08308e0d24cb08d02
root <jesus4388>
2022-08-19 10:01:48 -0700

    Response header value

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-# a Python script that takes in a URL
+'# a Python script that takes in a URL'
 import urllib.request
 import sys
 
 if __name__ == "__main__":
+
     with urllib.request.urlopen(sys.argv[1]) as response:
         print(response.info().get("X-Request-Id"))
