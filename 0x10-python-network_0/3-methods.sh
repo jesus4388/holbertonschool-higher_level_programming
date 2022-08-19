20a5ca0bcef5b7fc74ac1e395283eba22cea86f5
root <jesus4388>
2022-08-18 17:16:33 -0700

    l

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #ash script that takes in a URL and displays all HTTP methods
-curl -sI -X -OPTIONS "$1" | grep -i Allow: | cut -d' ' -f2-
+curl -sI -X -OPTIONS "$1" | grep Allow: | cut -d' ' -f2-
