eebfe9e0bc717126dbccf9d9100a1f27dd73b82e
root <jesus4388>
2022-08-19 19:20:01 -0700

    l

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -7,6 +7,6 @@ if __name__ == "__main__":
 
     x = requests.get(argv[1])
     if x.status_code < 400:
-	print(x.text)
+        print(x.text)
     else:
-	print("Error code: {:}".format(x.status_code))
+        print("Error code: {:}".format(x.status_code))
