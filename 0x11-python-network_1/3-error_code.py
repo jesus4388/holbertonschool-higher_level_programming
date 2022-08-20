e1b6710b4859d2e24e4a6c25488bae07bfc8c403
root <jesus4388>
2022-08-19 17:48:56 -0700

    l

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+'# python script that takes in a URL and an email'
+from sys import argv
+import urllib.request
+from urllib.error import HTTPError
+
+if __name__ == "__main__":
+    try:
+        with urllib.request.urlopen(argv[1]) as resp:
+            response = resp.read()
+            print(response.decode())
+    except HTTPError as err:
+        print("Error code: {}".format(err.code))
