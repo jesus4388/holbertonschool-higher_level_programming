b95b3d509746f53910692a1605c2bc53b98fdce1
jesus4388 <jesushernandez1843@gmail.com>
2022-09-12 10:25:03 -0700

    prints the content of a file

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+/**
+ * leer contenido de un archivo
+ *
+ */
+const fs = require('fs');
+
+fs.readFile(process.argv[2], 'utf8', (error, datos) => {
+  if (error)
+    console.log(error);
+  else
+    console.log(datos);
+});
