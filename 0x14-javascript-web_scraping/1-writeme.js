a337f3582d9c21874d2b14b732cacf8d2aaecaaa
jesus4388 <jesushernandez1843@gmail.com>
2022-09-12 11:03:20 -0700

    write me

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -3,9 +3,10 @@
  * a script that writes a string to a file
  *
  */
-const fs = require('fs')
+const fs = require('fs');
 
 fs.writeFile(process.argv[2], process.argv[3], error => {
-  if (error)
-    console.log(error)
-})
+  if (error) {
+    console.log(error);
+  }
+});
