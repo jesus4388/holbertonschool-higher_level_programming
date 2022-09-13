b6e207d505a340564f2b43bf5b72851d33958587
jesus4388 <jesushernandez1843@gmail.com>
2022-09-12 19:01:00 -0700

    How many completed?

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -3,10 +3,10 @@
  * a script that computes the number of task completed
  *
  */
-const axios = require('axios').default
+const axios = require('axios').default;
 let data;
 let dat;
-let dict = {};
+const dict = {};
 let value = 0;
 let id = 0;
 
@@ -14,16 +14,16 @@ axios.get(process.argv[2])
   .then(response => {
     data = response.data;
     for (dat in data) {
-      if (id != data[dat].userId) {
+      if (id !== data[dat].userId) {
         value = 0;
-	id = data[dat].userId;
+        id = data[dat].userId;
       }
       if (data[dat].completed) {
-	dict[data[dat].userId] = value += 1;
+        dict[data[dat].userId] = value += 1;
       }
     }
-    console.log(dict)
-    })
+    console.log(dict);
+  })
   .catch(error => {
     console.log(error);
   });
